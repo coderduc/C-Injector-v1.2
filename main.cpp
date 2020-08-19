0a69e7e49bf1e12d6b4a15a143c846ab2cbdd53d
CoderDuc <tuanloantuduc123@gmail.com>
2020-08-19 13:08:00 +0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <Windows.h>
+#include <TlHelp32.h>
+
+using namespace std;
+
+DWORD getPID(const char* procName)
+{
+	DWORD pid = 0;
+	HANDLE pProcess = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+	if (pProcess != INVALID_HANDLE_VALUE)
+	{
+		PROCESSENTRY32 procEntry;
+		procEntry.dwSize = sizeof(procEntry);
+		do
+		{
+			if (!_stricmp(procEntry.szExeFile, procName))
+			{
+				pid = procEntry.th32ProcessID;
+				cout << procEntry.szExeFile << " found - PID: " << pid << endl;
+				CloseHandle(pProcess);
+			}
+		} while (Process32Next(pProcess, &procEntry));
+	}
+	return pid;
+}
+
+bool InjectDLL(DWORD procId, const char* dllName)
+{
+	//Definations
+	char buff[MAX_PATH];
+	//Get Full Path
+	if (!procId) return false;
+	GetFullPathName(dllName, strlen(buff) + 1, buff, 0); // Get Full Path Of The File
+	cout << "Dll Path: " << buff << endl;
+	Sleep(1000);
+	//Injecting
+	HANDLE hProcess = OpenProcess(PROCESS_ALL_ACCESS, 0, procId); //Open targer process
+	LPVOID loc = VirtualAllocEx(hProcess, 0, strlen(buff) + 1, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE); // Create a virtual memory in targer process
+	Sleep(1000);
+	cout << "DLL path was allocated at: " << loc << endl;
+	WriteProcessMemory(hProcess, loc, buff, strlen(buff) + 1, 0); //Write Dll Path into &loc
+	HANDLE hThread = CreateRemoteThread(hProcess, 0, 0, (LPTHREAD_START_ROUTINE)LoadLibraryA, loc, 0, 0); // Load DLL
+	return true;
+}
+
+int main(int agrc,char* agrv[]) {
+
+	HANDLE hOutput = GetStdHandle(STD_OUTPUT_HANDLE);
+	SetConsoleTitleA("C-Inject v1.2");
+
+	if (!InjectDLL(getPID(agrv[1]), agrv[2])) {
+		cout << "An error while injecting dll !!!" << endl;
+	}
+	else {
+		cout << "DLL Injected !!!" << endl;
+	}
+	system("pause");
+	return 0;
+}
